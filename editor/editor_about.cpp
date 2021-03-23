4cede83d36c1aedcb9f3a848c505d83f578c3c3b
Diogo Carneiro <diogoncarneiro@outlook.com>
2021-03-23 14:40:55 +0000

    Editor about fixed.

--- editor/editor_about.cpp
+++ editor/editor_about.cpp
@@ -283,8 +283,8 @@ EditorAbout::EditorAbout() {
 	"\n"
 	"[3.2.3]\n"
 	"\n"
-		"- [EN] - Initial build with Spine 2D plugin.\n"
-		"- [PT] - Build inicial com o plugin Spine 2D.\n"
+		"- [EN] - Initial build With Spine Module Build-In.\n"
+		"- [PT] - Build inicial com o Spine Modulo Embutido.\n"
 	"\n"
 	"Animation brings video games alive. We believe creating great 2D animation requires\n"
 	"not only powerful software, but a powerful workflow. Spine is dedicated to 2D skeletal\n"
@@ -294,7 +294,8 @@ EditorAbout::EditorAbout() {
 	"\n"
 	"About the license\n"
 	"\n"
-	"This module is forked from sanikoyes's godot branch and some of the code is forked from godot-spine-module. Both of the code are declared as MIT license.\n"
+	"This module is forked from sanikoyes's godot branch and some of the code is forked from godot-spine-module.\n"
+	"Both of the code are declared as MIT license.\n"
 	"The license of this module is under the Spine Runtimes Software License.\n"
 	));
 	tc->add_child(_gamevolutions_text);
